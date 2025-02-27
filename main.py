0883e7d4bf85493597de9985948a5952a2b299f7
Yarik Popov <89220488+Yarik-Popov@users.noreply.github.com>
2025-02-26 21:20:41 -0500

    Create Linux port (#1)

--- main.py
+++ main.py
@@ -10,42 +10,50 @@ from gui import run_window
 import time
 
 
+write_mode("normal")  # Intialize Mode
 
-write_mode("normal") #Intialize Mode
 
-def load_image(): # Function to load Tray icon
+def load_image():  # Function to load Tray icon
     with open("Windows_Mouse_Movments/vimmode.txt", "r") as f:
         typ = f.read().strip()
-
 
     images = {
         "normal": "Images/omnivimn.png",
         "visual": "Images/omnivimv.png",
         "insert": "Images/omnivimi.png",
-        "mouse": "Images/omnivimm.png"
+        "mouse": "Images/omnivimm.png",
     }
 
-    if typ not in images:# Catch errors
+    if typ not in images:  # Catch errors
         return Image.open("Images/omnivim.png")
     return Image.open(images[typ])
 
-def run_code(): # Start program
-    if os.name == 'nt':
-        with open("Windows_Mouse_Movments/vimmode.txt", "w") as f:
-            f.truncate(0)
-            f.write("normal")
-        f.close()
-        t1 = Thread(target=run_windows, daemon=True)
-        t1.start()
 
-icon = pystray.Icon("omnivim", load_image(), menu=pystray.Menu(
-    Item("Open", run_window, default=True),
-))
+def run_code():  # Start program
+    # if os.name == "nt":
+    with open("Windows_Mouse_Movments/vimmode.txt", "w") as f:
+        f.truncate(0)
+        f.write("normal")
+    f.close()
+    t1 = Thread(target=run_windows, daemon=True)
+    t1.start()
+
+
+icon = pystray.Icon(
+    "omnivim",
+    load_image(),
+    menu=pystray.Menu(
+        Item("Open", run_window, default=True),
+    ),
+)
+
 
 def setup(icon):
     icon.visible = True
     while icon.visible:  # Ensure the thread stops if the icon is closed
         icon.icon = load_image()
         time.sleep(0.1)  # Avoid 100% CPU usage
-run_code() 
+
+
+run_code()
 icon.run(setup)
